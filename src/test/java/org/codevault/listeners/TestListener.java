e102a3083b8f8ef690b36b1847de6d00fe925656
Suhas <suhas7734@gmail.com>
2025-05-16 20:48:05 +0530

    created reporting mechanism

--- src/test/java/org/codevault/listeners/TestListener.java
+++ src/test/java/org/codevault/listeners/TestListener.java
@@ -2,37 +2,44 @@ package org.codevault.listeners;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
+import org.codevault.reports.ExtentLogger;
+import org.codevault.reports.ExtentReport;
+import org.testng.*;
 
-public class TestListener implements ITestListener {
+public class TestListener implements ITestListener, ISuiteListener {
 
     private static final Logger logger = LogManager.getLogger(TestListener.class);
 
-    public void onStart(ITestContext context) {
+    public void onStart(ISuite context) {
+        ExtentReport.initReports();
         logger.info("Test Suite Started");
     }
 
-    public void onFinish(ITestContext context) {
+    public void onFinish(ISuite context) {
+        ExtentReport.flushReports();
         logger.info("Test Suite Completed!");
     }
 
     public void onTestStart(ITestResult result) {
+        ExtentReport.createTest(result.getName());
         logger.info("Test Started!! " + result.getMethod().getMethodName());
         logger.info("Description!! " + result.getMethod().getDescription());
     }
 
     public void onTestSuccess(ITestResult result) {
+        ExtentLogger.pass(result.getName() + " is passed");
         logger.info("Test Passed!! " + result.getMethod().getMethodName());
+
     }
 
     public void onTestFailure(ITestResult result) {
+        ExtentLogger.fail(String.valueOf(result.getThrowable()));
         logger.error("Test Failed!! " + result.getMethod().getMethodName());
         logger.error("Failure Description!! " + result.getMethod().getDescription());
     }
 
     public void onTestSkipped(ITestResult result) {
+        ExtentLogger.skip(result.getName() + " is skipped");
         logger.info("Skipped!! " + result.getMethod().getMethodName());
     }
 }
